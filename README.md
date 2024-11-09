daeb7760402994d5b4ef6651a71869c074d2118b
Juliana Pellegrini Reis <20231590@novaims.unl.pt>
2024-11-09 01:38:46 +0000

    Updated README and added basic files
    Updates the README file.
    
    Adds the files config, main, interface, and utils made during the Computation 3 practical classes by week 8.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-Group-1-Computation-3
-Final Project for Computation 3 made by Group 1
+
+Final Project for Computation 3 made by Group 1 for the NOVA IMS Information Systems Course. 
+
+Group 1 consists of students Madalena Duarte (), André Calheiros (), Júlia Vidal () and Juliana Reis (20231590).
---
+++ config.py
@@ -0,0 +1,12 @@
+# Config file used to set global variables and other settings
+# COLORS AND PICTURES HERE FOR NOW
+
+# COLORS
+dark_red = (138, 0, 0)  # Dark red for buttons
+deep_black = (19, 20, 20)  # Almost black for background
+grey = (59, 60, 60)  # Dark grey for alternate buttons
+white = (254, 255, 255)  # White for readable text
+glowing_light_red = (239, 128, 128)  # Light red for brighter text
+
+# SCREEN RESOLUTION
+resolution = (720, 720) # height/width
