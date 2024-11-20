0c1b008bc4aaae268be0e7b9f2cf4e9cbbe950c5
Madalena Duarte <20231614@novaims.unl.pt>
2024-11-20 14:08:13 +0000

    Adicionei cores (pallete azul)
    linha 15 - 20:
    5 tons de azul

--- config.py
+++ config.py
@@ -8,15 +8,23 @@ grey = (59, 60, 60)  # Dark grey for alternate buttons
 white = (254, 255, 255)  # White for readable text
 glowing_light_red = (239, 128, 128)  # Light red for brighter text
 blue = (0, 0, 255)
-forest_green = (34, 139, 34)
-red = (255, 0, 0)
+green = (34, 139, 34)
+yellow = (255, 255, 0)
+red = (150, 0, 24)
+
+# CUSTOMIZED COLORS
+oxford_blue = (0, 7, 45)
+penn_blue = (0, 28, 85)
+royal_blue = (10, 36, 114)
+bice_blue = (14, 107, 168)
+light_blue = (166, 225, 250)
 
 # SCREEN RESOLUTION
-resolution = (720, 720)  # height/width
+resolution = (720, 720) # height/width
 width, height = resolution[0], resolution[1]
-fps = 60  # frames per second
+fps = 60
 
 # SIZES
 player_size = (50, 100)
-enemy_size = 40
+enemy_size = (40, 40)
 bullet_size = 10
