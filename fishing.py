24a2ef87effa6f9c3c6d18817b40e37c073dcab0
Juliana Pellegrini Reis <20231590@novaims.unl.pt>
2024-12-05 06:35:30 +0000

    Update fishing and fishes

--- fishing.py
+++ fishing.py
@@ -1,6 +1,8 @@
 from utils import *
 from config import *
 from button import Button, select_sound
+from fishes import Salmon, Cod
+import random
 
 
 def fishing():
@@ -64,9 +66,20 @@ def fishing_minigame():
     # setting up the clock for fps
     clock = pygame.time.Clock()
 
-    # setting up the buttons
-    back_button = Button(1000, 650, 150, 60, "Back", None, "chiller", 35, True, bice_blue,
-                         image="images/ice-banner.png")
+    # Hide the mouse cursor
+    pygame.mouse.set_visible(False)
+
+    # Define the fish types
+    fish_types = [Salmon, Cod]
+
+    # Function to spawn a fish based on probabilities
+    def spawn_fish():
+        fish_selected = random.choices(fish_types, [little_guy().probability for little_guy in fish_types])[0]
+        return fish_selected()
+
+    # Create the fish
+    current_fish = spawn_fish()
+    fish_group = pygame.sprite.Group(current_fish)
 
     running = True
     while running:
@@ -81,17 +94,19 @@ def fishing_minigame():
             if ev.type == pygame.QUIT:
                 pygame.quit()
 
-            if back_button.is_clicked(mouse, ev):
-                select_sound()
-                return
-
-        # drawing the buttons
-        back_button.draw(screen, mouse)
+            if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+                pygame.mouse.set_visible(True)
+                return  # return to the fishing screen
 
         # drawing the fishing minigame
 
         # drawing the fishing bar
-        draw_chasing_rectangle(screen, mouse, 20, 100, white, pink)
+        draw_chasing_rectangle(screen, mouse, 30, 100, white, pink)
+
+        # drawing the fish
+        for fish in fish_group:
+            fish.update_position()
+            screen.blit(fish.image, fish.rect)
 
         # updating the display
         pygame.display.update()
