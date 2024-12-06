657dd256094fc3670062889a7438fa601527a525
Juliana Pellegrini Reis <20231590@novaims.unl.pt>
2024-12-06 01:30:37 +0000

    Update weapon selector

--- igloo/weapon_selector.py
+++ igloo/weapon_selector.py
@@ -22,4 +22,43 @@ def weapon_selector():
     select_button = Button(1000, 550, 150, 60, "Select", None, "chiller", 35, True, bice_blue,
                            image="images/ice-banner.png")
 
+    # set running
+    running = True
+    while running:
 
+        # set fps
+        clock.tick(fps)
+
+        # display background
+        screen.blit(background, (0, 0))
+
+        # get mouse position
+        mouse = pygame.mouse.get_pos()
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                running = False
+
+            if back_button.is_clicked(mouse, ev):
+                select_sound()
+                return
+
+            if select_button.is_clicked(mouse, ev):
+                select_sound()
+                under_construction()
+
+            if back_button.is_hovered(mouse):
+                back_button.scale_up()
+            else:
+                back_button.scale_down()
+
+            if select_button.is_hovered(mouse):
+                select_button.scale_up()
+            else:
+                select_button.scale_down()
+
+        # draw buttons
+        back_button.draw(screen, mouse)
+        select_button.draw(screen, mouse)
+
+        pygame.display.update()
