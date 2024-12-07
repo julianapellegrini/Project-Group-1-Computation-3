14dd06c5185aec275801973556e9d7cbefafa7bf
Juliana Pellegrini Reis <20231590@novaims.unl.pt>
2024-12-07 21:42:06 +0000

    Despawner update
    Give back despawner functions removed

--- powerups/despawner.py
+++ powerups/despawner.py
@@ -10,8 +10,19 @@ class DeSpawner(PowerUp):
     def affect_player(self, surface, player):
         pass
 
-    def affect_game(self):
-        pass
+    def affect_game(self, surface, spawn_rate, player):
+        # The affect game logic is in this method
+        self.active = True
+        self.start_time = time.time()
+        if self.active:
+            # Position the power-up image around the player_related
+            self.image_rect.center = player.rect.center
+            surface.blit(self.image, self.image_rect.topleft)
+            # Check if the power-up has been active for 5 seconds
+            if self.active and time.time() - self.start_time >= 5:
+                self.deactivate()
+        return spawn_rate * self.reduction_factor
 
-    def deactivate(self):
-        pass
+    def deactivate(self, spawn_rate):
+        self.active = False
+        return spawn_rate / self.reduction_factor
