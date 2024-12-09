d15a0931b03be003888fe7c5e0d79650d6513821
Juliana Pellegrini Reis <20231590@novaims.unl.pt>
2024-12-09 15:48:14 +0000

    Bullet Update
    Bullets are now fish :)
    
    Bullet image changes depending on the direction its going

--- player_related/bullet.py
+++ player_related/bullet.py
@@ -11,6 +11,7 @@ class Bullet(pygame.sprite.Sprite):
         self.direction = direction
         self.radius = bullet_size
         self.color = yellow
+        self.image = None  # none for now, changes later depending on the direction
 
         self.rect = pygame.Rect(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
         self.speed = 8
@@ -26,5 +27,23 @@ class Bullet(pygame.sprite.Sprite):
             self.kill()
 
     def draw(self, screen):
-        # drawing the bullet on screen
-        pygame.draw.circle(screen, self.color, self.rect.center, self.radius)
+        # bullet image changes based on the direction of the bullet
+        if self.direction == 0:
+            image = pygame.image.load('images/bullet_right.png')
+            image = pygame.transform.scale(image, (bullet_size, bullet_size))
+            self.image = image
+        elif self.direction == math.pi / 2:
+            image = pygame.image.load('images/bullet_down.png')
+            image = pygame.transform.scale(image, (bullet_size, bullet_size))
+            self.image = image
+        elif self.direction == math.pi:
+            image = pygame.image.load('images/bullet_left.png')
+            image = pygame.transform.scale(image, (bullet_size, bullet_size))
+            self.image = image
+        elif self.direction == 3 * math.pi / 2:
+            image = pygame.image.load('images/bullet_up.png')
+            image = pygame.transform.scale(image, (bullet_size, bullet_size))
+            self.image = image
+
+        # drawing the bullet on the screen
+        screen.blit(self.image, self.rect)
